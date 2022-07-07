dda8814f6152b92889b55b770c63b85476edb07c
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-07-07 16:43:21 +0530

    Initial commit

---
+++ src/main/java/java2wsdl/model/DepartmentList.java
@@ -0,0 +1,18 @@
+package java2wsdl.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentList {
+	List<Department> departments = new ArrayList<Department>();
+
+	public List<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
+	}
+	
+
+}
